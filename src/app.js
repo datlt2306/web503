67418034b2c41a512b8a7f4cd9456489e0b7be67
datlt2306 <datlt2306@gmail.com>
2024-03-04 13:00:54 +0700

    Buổi 1 - commit 2
    Khai báo server sử dụng createServer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 // export const viteNodeApp = app;
 
+import http from 'http';
+const server = http.createServer(() => {
+    console.log('ahihi')
+});
+
+server.listen(8080, () => {
+    console.log('conntect success!')
+});
+
 // B1: npm i vite vite-plugin-node -D
 // B2: tạo file vite.config.js ở root -> copy code của thầy
 // B3: chỉnh sửa file package.json -> copy code của thầy
