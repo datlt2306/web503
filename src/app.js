5ae5edee0ef9955d2c8ec25fb25a43fae7cfe65a
datlt2306 <datlt2306@gmail.com>
2024-03-04 12:27:05 +0700

    Buổi 1 - commit 1
    Setup môi trường

--- src/app.js
+++ src/app.js
@@ -1,23 +1,4 @@
-import express from "express";
-import productRouter from "./routes/product";
-import categoryRouter from "./routes/category";
-import authRouter from "./routes/auth";
-import cors from "cors";
-import mongoose from "mongoose";
-
-const app = express();
-
-// middleware
-app.use(express.json());
-app.use(cors());
-
-// router
-app.use("/api", productRouter);
-app.use("/api", categoryRouter);
-app.use("/api", authRouter);
-mongoose.connect("mongodb://localhost:27017/we17303");
-
-export const viteNodeApp = app;
+// export const viteNodeApp = app;
 
 // B1: npm i vite vite-plugin-node -D
 // B2: tạo file vite.config.js ở root -> copy code của thầy
