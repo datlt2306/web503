af6490172e95d34734ad21655f3a99c544541ab8
datlt2306 <datlt2306@gmail.com>
2024-03-06 12:32:25 +0700

    WD18328 - Buổi 2 - commit 1
    Cài đặt môi trường

--- src/app.js
+++ src/app.js
@@ -1,25 +1,22 @@
-// export const viteNodeApp = app;
-// import http from 'http';
-import express from 'express';
-
-const app = express();
-
-app.get('/api/products', (req, res) => {
-    const data = [
-        { id: 1, name: "Product A"},
-        { id: 2, name: "Product B"}
-    ];
-    res.json(data);
-});
-app.listen(8080, () => {
-    console.log('conntect success!')
-});
-
 // B1: npm i vite vite-plugin-node -D
 // B2: tạo file vite.config.js ở root -> copy code của thầy
 // B3: chỉnh sửa file package.json -> copy code của thầy
 // B4: chỉnh sửa file app.js -> export const viteNodeApp = app; ở cuối file
 // B5: npm run dev
 
-
 // Cài đặt express: npm i express --save
+
+import http from "http";
+import express from 'express';
+
+const app = express();
+
+app.get(`/api/products`, (req, res) => {
+    res.json([{id: 1, name: "Product A"}])
+})
+
+app.listen(8080, () => {
+    console.log('Started')
+})
+
+export const viteNodeApp = app;
