672a9513d98389212cb6c69f3a60ad5519cb0f45
datlt2306 <datlt2306@gmail.com>
2024-03-07 14:29:10 +0700

    Commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 const app = express();
+
+// middleware
+app.use(express.json());
+
+
 // router
 // path -> endpoint
 // restful API
@@ -12,29 +17,76 @@ app.get('/api/products', async(req, res) => {
         // tra ve client
         res.json(data);
     } catch (error) {
-        
+        res.status(500).json({message: error.message});
     }
-    
 });
-app.get('/api/products', (req, res) => {
-    res.json([
-        {id: 1, name: "Product A"}
-    ])
+// Chi tiết sản phẩm
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        // fake connect database
+        const response = await fetch(`http://localhost:3000/products/${req.params.id}`);
+        const data = await response.json();
+        // tra ve client
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
-app.post('/api/products', (req, res) => {
-    res.json([
-        {id: 1, name: "Product A"}
-    ])
+// thêm sản phẩm
+app.post('/api/products', async (req, res) => {
+    try {
+        // fake connect database
+        const response = await fetch(`http://localhost:3000/products`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req.body)
+        });
+        const data = await response.json();
+        // tra ve client
+        res.status(201).json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
-app.put('/api/products', (req, res) => {
-    res.json([
-        {id: 1, name: "Product A"}
-    ])
+// Xóa sản phẩm
+app.delete('/api/products/:id', async (req, res) => {
+    try {
+        // fake connect database
+        const response = await fetch(`http://localhost:3000/products/${req.params.id}`, {
+            method: "DELETE",
+        });
+        const data = await response.json();
+        // tra ve client
+        res.status(200).json({
+            message: "Xóa sản phẩm thành công",
+            data
+        });
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
-app.delete('/api/products', (req, res) => {
-    res.json([
-        {id: 1, name: "Product A"}
-    ])
+
+app.put('/api/products/:id', async (req, res) => {
+    try {
+        // fake connect database
+        const response = await fetch(`http://localhost:3000/products/${req.params.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req.body)
+        });
+        const data = await response.json();
+        // tra ve client
+        res.status(200).json({
+            message: "Cập nhật sản phẩm thành công",
+            data
+        });
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 app.listen(8080);
 
